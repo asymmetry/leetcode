3922a4746e6b93ce3b42b714f5a00d35fb7cfc0c
Chao Gu <guchao.pku@gmail.com>
2026-06-15 19:45:15 -0700

    3 & 4

---
+++ 0003_longest_substring_without_repeating_characters/solution.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+auto speedup = []() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  return 0;
+}();
+
+class Solution {
+ public:
+  int lengthOfLongestSubstring(string s) {
+    if (s.empty()) {
+      return 0;
+    }
+
+    int current_len = 1;
+    int max_len = 1;
+    for (int i = 1; i < (int)s.size(); i++) {
+      int begin = i - current_len;
+      current_len++;
+      for (int j = i - 1; j >= begin; j--) {
+        if (s[j] == s[i]) {
+          current_len = i - j;
+          max_len = std::max(max_len, current_len);
+          break;
+        }
+      }
+      max_len = std::max(max_len, current_len);
+    }
+
+    return std::max(max_len, current_len);
+  }
+};
+
+int main() {
+  string s = "abcabcbb";
+
+  Solution solution;
+  auto result = solution.lengthOfLongestSubstring(s);
+
+  std::cout << result << std::endl;
+
+  return 0;
+}
---
+++ 0004_median_of_two_sorted_arrays/solution.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+auto speedup = []() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  return 0;
+}();
+
+class Solution {
+ public:
+  double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    int l1 = (int)nums1.size();
+    int l2 = (int)nums2.size();
+    int l0 = l1 + l2;
+
+    if (l0 == 0) return 0.0;
+
+    int p1 = 0, p2 = 0;
+    int v1 = -1000000, v2 = -1000000;
+    while (p1 + p2 < (l0 / 2 + 1)) {
+      if (p1 >= l1) {
+        v2 = v1;
+        v1 = nums2[p2];
+        p2++;
+        continue;
+      }
+      if (p2 >= l2) {
+        v2 = v1;
+        v1 = nums1[p1];
+        p1++;
+        continue;
+      }
+
+      if (nums1[p1] <= nums2[p2]) {
+        v2 = v1;
+        v1 = nums1[p1];
+        p1++;
+      } else {
+        v2 = v1;
+        v1 = nums2[p2];
+        p2++;
+      }
+    }
+
+    if (l0 % 2 == 0) {
+      return ((double)v1 + (double)v2) / 2.0;
+    } else {
+      return (double)v1;
+    }
+  }
+};
+
+int main() {
+  vector<int> nums1 = {1, 3};
+  vector<int> nums2 = {2};
+
+  Solution solution;
+  auto result = solution.findMedianSortedArrays(nums1, nums2);
+
+  std::cout << result << std::endl;
+
+  return 0;
+}
