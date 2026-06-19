350433d6a65e710d61bceef211e9a86959e169f9
Chao Gu <guchao.pku@gmail.com>
2026-06-19 15:43:58 -0700

    15 & 16

---
+++ 0015_3sum/solution.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+auto speedup = []() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  return 0;
+}();
+
+class Solution {
+ public:
+  vector<vector<int>> threeSum(vector<int>& nums) {
+    int len = (int)nums.size();
+
+    std::unordered_map<int, int> map;
+    for (int i = 0; i < len; i++) {
+      if (map.count(nums[i]) != 0) {
+        map[nums[i]]++;
+      } else {
+        map[nums[i]] = 1;
+      }
+    }
+
+    vector<vector<int>> result;
+
+    for (const auto& x : map) {
+      int xx = x.first;
+      for (const auto& y : map) {
+        int yy = y.first;
+        if (xx < yy || (xx == yy && x.second >= 2)) {
+          int zz = 0 - xx - yy;
+          if (map.count(zz) == 0 || (zz < xx) || (zz < yy) ||
+              (zz == xx && x.second < 2) || (zz == yy && y.second < 2) ||
+              (zz == xx && zz == yy && x.second < 3)) {
+            continue;
+          }
+          result.push_back(vector<int>{xx, yy, zz});
+        }
+      }
+    }
+
+    return result;
+  }
+};
+
+int main() {
+  vector<int> nums = {-1, 0, 1, 2, -1, -4};
+
+  Solution solution;
+  auto result = solution.threeSum(nums);
+
+  std::cout << "[";
+  for (int i = 0; i < (int)result.size(); ++i) {
+    std::cout << "[";
+    for (int j = 0; j < (int)result[i].size(); ++j) {
+      if (j != (int)result[i].size() - 1) {
+        std::cout << result[i][j] << ",";
+      } else {
+        std::cout << result[i][j];
+      }
+    }
+    if (i != (int)result.size() - 1) {
+      std::cout << "],";
+    } else {
+      std::cout << "]";
+    }
+  }
+  std::cout << "]" << std::endl;
+
+  return 0;
+}
---
+++ 0016_3sum_closest/solution.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+auto speedup = []() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  return 0;
+}();
+
+class Solution {
+ public:
+  int threeSumClosest(vector<int>& nums, int target) {
+    int len = (int)nums.size();
+
+    std::sort(nums.begin(), nums.end());
+
+    int min_diff = 1000000;
+    int min_target = 0;
+    for (int i = 1; i < len - 1; i++) {
+      int l = 0, r = len - 1;
+
+      while (r > i && i > l) {
+        int sum = nums[l] + nums[i] + nums[r];
+        int diff = sum - target;
+
+        if (std::abs(diff) < min_diff) {
+          min_diff = std::abs(diff);
+          min_target = sum;
+        }
+
+        if (diff == 0) return target;
+
+        if (diff > 0) {
+          r--;
+        } else {
+          l++;
+        }
+      }
+    }
+
+    return min_target;
+  }
+};
+
+int main() {
+  vector<int> nums = {-1,2,1,-4};
+  int target = 1;
+
+  Solution solution;
+  auto result = solution.threeSumClosest(nums, target);
+
+  std::cout << result << std::endl;
+
+  return 0;
+}
